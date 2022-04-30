0e069e82c0c7f409d30e7c1714a8cad1c65ab42d
tanacchi <q111026d@mail.kyutech.jp>
2022-04-30 16:50:10 +0900

    Setup plane spring boot workspace

---
+++ src/main/java/com/example/tanakinator2/Tanakinator2Application.java
@@ -0,0 +1,13 @@
+package com.example.tanakinator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tanakinator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tanakinator2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tanakinator2/Tanakinator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tanakinator2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tanakinator2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
